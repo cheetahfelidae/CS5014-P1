3e957d820b14ad2bde7e6e5f16ff9549bcbb8a13
cheetah <inchaiya.keta@gmail.com>
2018-03-13 17:12:31 +0000

    1. loading and cleaning the data, 2. analysing and visualising the data, 3. preparing the inputs and choosing a suitable subset of features 4. selecting and training a regression model.

---
+++ clean_data.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+import matplotlib.pyplot as plt
+
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+
+def calculate_nms(dt_obj):
+    tomorrow = dt_obj + timedelta(1)
+    midnight = datetime(year=tomorrow.year, month=tomorrow.month, day=tomorrow.day, hour=0, minute=0, second=0)
+    return (midnight - dt_obj).seconds
+
+
+energy = pd.read_csv('/Users/cheetah/Sites/CS5014-P1/energydata_complete.csv')
+nsm_list = []
+energy["nsm"] = ""
+for index, row in energy.iterrows():
+    dt_str = row["date"]
+    dt_obj = datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
+    nsm_list.append(calculate_nms(dt_obj))
+energy["nsm"] = nsm_list
+energy.drop(["rv1", "rv2", "date"], axis=1, inplace=True)
+
+train_set, test_set = train_test_split(energy, test_size=0.2, random_state=42)
+X_train = train_set.drop("Appliances", axis=1)
+Y_train = train_set["Appliances"].copy()
+
+full_pipeline = Pipeline([
+    ("selector", DataFrameSelector(list(X_train))),
+    ("std_scaler", StandardScaler())
+])
+
+X_train = full_pipeline.fit_transform(X_train)
+np.savetxt("X_train", X_train)
+np.savetxt("Y_train", Y_train)
+
+energy.hist(bins=50, figsize=(20, 15))
+# energy.info()
+print(energy.describe())
+plt.show()
