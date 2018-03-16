95058bc64aa3fdeec55a97509ad031047b565970
cheetah <inchaiya.keta@gmail.com>
2018-03-16 15:25:35 +0000

    Comments and cleaned code

--- DataFrameSelector.py
+++
@@ -1,9 +0,0 @@
-class DataFrameSelector():
-    def __init__(self, attributeNames):
-        self.attributeNames = attributeNames
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return X[self.attributeNames].values
--- reg_model_trainer.py
+++ reg_model_trainer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
@@ -11,23 +12,23 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Training.')
 
-parser.add_argument('-ln', action='store_true', help='Use the use linear regression model')
-parser.add_argument('-rid', action='store_true', help='Use the use linear regression model')
-parser.add_argument('-poly', action='store_true', help='Use the use linear regression model')
-parser.add_argument('-sgd', action='store_true', help='Use the use linear regression model')
+parser.add_argument('-lin', action='store_true', help='Linear Regression Model')
+parser.add_argument('-rid', action='store_true', help='Ridge Regression Model')
+parser.add_argument('-pol', action='store_true', help='Polynomial Regression Model')
+parser.add_argument('-sto', action='store_true', help='Stochastic Gradient Descent Model')
 
 args = parser.parse_args()
 
-''' ################### Training ##################### '''
+''' *********************************************** Training Part *********************************************** '''
 training_x = np.loadtxt('training_x.txt')
 training_y = np.loadtxt('training_y.txt')
 
-if args.ln is True:
-    print("Use linear regression model")
+if args.lin is True:
+    print("Linear Regression Model")
     reg_model = LinearRegression()
 
 elif args.rid is True:
-    print("Use Ridge regression model")
+    print("Ridge Regression Model")
     full_pipe_line = Pipeline([
         ("polynomial", PolynomialFeatures(degree=2, include_bias=False)),
         ("stf_scale", StandardScaler())
@@ -36,8 +37,8 @@ elif args.rid is True:
     training_x = full_pipe_line.fit_transform(training_x)
     reg_model = Ridge()
 
-elif args.poly is True:
-    print("Use Polynomial regression model")
+elif args.pol is True:
+    print("Polynomial Regression Model")
     full_pipe_line = Pipeline([
         ("polynomial", PolynomialFeatures(degree=2, include_bias=False)),
         ("stf_scale", StandardScaler())
@@ -45,8 +46,8 @@ elif args.poly is True:
 
     training_x = full_pipe_line.fit_transform(training_x)
     reg_model = LinearRegression()
-elif args.sgd is True:
-    print("Use Stochastic Gradient Descent model")
+elif args.sto is True:
+    print("Stochastic Gradient Descent Model")
     full_pipe_line = Pipeline([
         ("stf_scale", StandardScaler())
     ])
@@ -62,30 +63,29 @@ y_hat = reg_model.predict(training_x)
 
 # Save into file
 joblib.dump(reg_model, "trained_reg.pkl")
+
+# evaluate score by cross validation
 scores = cross_val_score(reg_model, training_x, training_y, scoring="neg_mean_squared_error", cv=10)
 rmse = np.sqrt(-scores)
 
 print("Training result")
-print("Scores", rmse)
-print("RMSE:", rmse.mean())
-print("Standard deviation", rmse.std())
+print("RMSE:", rmse)
+print("RMSE (mean): %0.2f (+/- %0.2f)" % (rmse.mean(), rmse.std() * 2))
+print("R^2: %0.2f" % (reg_model.score(training_x, training_y)))
 
-# trainingX = np.c_[np.ones_like(trainingX),trainingX]
-print('MSE = ', mean_squared_error(training_y, y_hat))
 
-''' ################ Testing ################## '''
+''' *********************************************** Testing Part *********************************************** '''
 testing_x = np.loadtxt('test_x.txt')
 testing_y = np.loadtxt('test_y.txt')
 
-# testingX = np.c_[np.ones_like(testingX),testingX]
-if args.ln is False:
+if args.lin is False:
     testing_x = full_pipe_line.fit_transform(testing_x)
 
 y_hat = reg_model.predict(testing_x)
 
-mse = mean_squared_error(testing_y, y_hat)
-rmse = np.sqrt(mse)
+rmse = np.sqrt(mean_squared_error(testing_y, y_hat))
 print("")
 print("Testing result")
-print("RMSE:", rmse.mean())
-print('MSE = ', mse)
+print("RMSE: %0.2f" % (rmse.mean()))
+print("R^2: %0.2f" % (reg_model.score(testing_x, testing_y)))
+print("")
